7b658a9455652fb29395f391ccb05d16f962bbbf
Martin Betz <martin.betz@visablelabs.com>
2021-06-30 17:24:33 +0200

    Add: Option to show or log stats

--- stats.php
+++ stats.php
@@ -18,3 +18,34 @@ $totalPrivate = $thingsDB->querySingle($totalPrivateQuery);
 $createdWork = $thingsDB->querySingle($createdWorkQuery);
 $doneWork = $thingsDB->querySingle($doneWorkQuery);
 $totalWork = $thingsDB->querySingle($totalWorkQuery);
+
+
+// Find command
+if (! isset($argv[1])) {
+    echo "Use log or show argument";
+    die();
+}
+
+if ($argv[1] == 'show') {
+    // Show results
+	echo "Today: " . $today . "\r\n" .
+	"Created Private: " . $createdPrivate . "\r\n" . 
+	"Done Private: " . $donePrivate . "\r\n" . 
+	"Total Private: " . $totalPrivate . "\r\n" .
+	"Created Work: " . $createdWork . "\r\n" . 
+	"Done Work: " . $doneWork . "\r\n" .
+	"Total Work: " . $totalWork . "\r\n" . 
+	"Total: " . $totalPrivate + $totalWork . "\r\n";
+}
+
+elseif ($argv[1] == 'log') {
+	// Enter results in stats db
+    $statsDB->exec("
+		insert into stats(date, created_private, done_private, total_private, created_work, done_work, total_work) 
+		VALUES('$today', $createdPrivate, $donePrivate, $totalPrivate, $createdWork, $doneWork, $totalWork)
+	");
+}
+
+else {
+    echo "Use log or show";
+}
